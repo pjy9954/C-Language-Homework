25f1ea8110fca08084ecfae2a011ec323d1a2024
예슬아씨 <lamb17171771@gmail.com>
2017-05-23 18:55:44 +0900

    Tuesday 23 may 2017 - 02

---
+++ study.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	char ch;
+	ch = 'A';
+	while ( ch <= 'Z')
+	{
+		printf( "%c", ch );
+		ch++;
+	}
+	return 0;
+	*/
+	char ch;
+	for ( ch = 'A'; ch <= 'Z'; ch++)
+	{
+		printf( "%c", ch );
+	}
+	return 0;
+}
